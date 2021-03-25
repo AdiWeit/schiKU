48defe539ae87abd218be39a36c8d0b532799739
AdiWeit <adi004@web.de>
2021-03-25 13:23:35 +0100

    Markierung bei Anklicken angepasst, comments added,

--- analysis.js
+++ analysis.js
@@ -20,7 +20,7 @@ function getAllGraphemtreffer(/*doNotMark, graphemFehler, correct*/changedByUser
     }
   }
   document.getElementById('allGraphemes' + selectedElementId.parent.toString().split('Sheet')[1]).innerHTML = '<a style="font-size:20">gesamt </a> <strong style="font-size:20">' + auswertung.allGraphemtreffer.got + '/' + auswertung.allGraphemtreffer.possible + "</strong> <a style='font-size:20'>Graphemtreffer</a>";
-  // getAllCorrectWords
+  // alle richtig verschriftlichen Wörter erfassen
   var correctWords = {possible: 0, got: 0};
   for (var i = 0; i < neededTest.words.length; i++) {
     correctWords.possible++;
@@ -32,6 +32,7 @@ function getAllGraphemtreffer(/*doNotMark, graphemFehler, correct*/changedByUser
 // Schreibdiagnostik: welcher Buchstabe bzw. Laut wie häufig richtig?
 // Datenübergabe und Befehl für Erstellung der Graphen
 function getEveryCategory(printMode) {
+  // Silben aus der Auswertung löschen (wird neu gezählt)
   for (auswertungNow of Object.keys(auswertung)) {
     if (auswertungNow.includes("Silben")/* != "letter" && auswertungNow != "allGraphemtreffer" && auswertungNow != "wrongLetters"*/) {
       delete auswertung[auswertungNow];
@@ -39,7 +40,7 @@ function getEveryCategory(printMode) {
     }
   }
   auswertung.letter[selectedElementId.parent] = {};
-
+  // doNotCount (Wörter mit manueller Korrektur durch Anpassung der Grapahemtreffer) Laute in minusList (Liste, welche Buchstaben wie Häufig auf Grund von Lauten (hier: mehr als 1 Buchstabe) oder weil sie nicht auswertbar sind von der Auswertung in den Rubriken ausgeschlossen werden müssen) hinzufügen
   var minusList = {doNotCount: {}};
   if (!auswertung.doNotCount[selectedElementId.parent]) auswertung.doNotCount[selectedElementId.parent] = [];
   for (var i = 0; i < auswertung.doNotCount[selectedElementId.parent].length; i++) {
@@ -53,7 +54,7 @@ function getEveryCategory(printMode) {
   }
   var doNotCountObj = JSON.parse(JSON.stringify(minusList));
       doNotCountObj.laute = {};
-  // Umschreibung in kategorien
+  // Umschreibung in kategorien (vorher: auswertung.categories[aktueller Test, also pupilSheet][Wort][Rubrik][Buchstabe] nachher: auswertung.byCategories[Rubrik][Buchstabe])
   auswertung.byCategories = {};
   for (var i1 = 0; auswertung.categories[selectedElementId.parent] && i1 < Object.keys(auswertung.categories[selectedElementId.parent]).length; i1++) {
     var wordNow = Object.keys(auswertung.categories[selectedElementId.parent])[i1];
@@ -73,7 +74,7 @@ function getEveryCategory(printMode) {
           // TODO: wenn keine Kathegorie vorhanden
           if (!doNotCountObj.laute[objKeyName]) doNotCountObj.laute[objKeyName] = 0;
           if (objKeyName.length == 1) doNotCountObj.laute[objKeyName] += doNotCountObj[objKeyName][wordNow];
-          // TODO: Laute: einzelne Buchstaben übertragen (aus Anzahl kann aus auswertung.categories abgelesen werden)
+          // TODO: Laute: einzelne Buchstaben übertragen (Anzahl kann aus auswertung.categories abgelesen werden)
           else doNotCountObj.laute[objKeyName]++;
         }
       }
@@ -135,13 +136,14 @@ function getEveryCategory(printMode) {
     if (!auswertung.byCategories.Endungen) auswertung.byCategories.Endungen = {};
     if (auswertung.letter[selectedElementId.parent]["<e>"]) auswertung.byCategories.Endungen["<e>/<E>"] = auswertung.letter[selectedElementId.parent]["<e>"];
   }
-  // zur Liste nach Kategorien und Alphabet geordnet hinzufügen
+  // Anzahl Richtige und Anzahl Falsche zur Liste nach Kategorien und Alphabet geordnet hinzufügen
   var categoryList = [];
   var wordsCountedTogether = [];
   auswertung.byCategories = sortObjectByKey(auswertung.byCategories, true);
   for (category of Object.keys(auswertung.byCategories)) {
     auswertung.byCategories[category] = sortObjectByKey(auswertung.byCategories[category], true);
     categoryList.push(category);
+    // Zusammenzählen der Rubrik, falls ausgewählt
   if (neededTest.countTogether && neededTest.countTogether.includes(category) && countTogether.checked) {
     categoryList[categoryList.length - 1] = "zusammen " + categoryList[categoryList.length - 1];
     rightList.push(auswertung.byCategories[category].got)
@@ -152,8 +154,10 @@ function getEveryCategory(printMode) {
     }
   }
   else {
+    // 0 wegen Text der Rubriken auf der y-Achse (dort sollen keine Balken sein)
     rightList.push(0)
     wrongList.push(0);
+    // Hinzufügen der Rubriken, Anzahl richtig und Anzahl falsch geschriebener Laute in Liste für Grafik hinzufügen
     for (objKeyName of Object.keys(auswertung.byCategories[category])) {
       if (objKeyName != "got" && objKeyName != "possible") {
         // letterList push weiter unten um Buchstaben, die in einer Kategorie sind aber in dem spezifisch gewählen Test nicht vorkommen abzufangen
@@ -165,11 +169,13 @@ function getEveryCategory(printMode) {
     }
   }
 }
+// alle zu bewertende Laute in "letterList" hinzufügen
 for (category of Object.keys(neededTest.kategorien)) {
   for (laut of neededTest.kategorien[category]) {
     letterList.push(laut);
   }
 }
+// Falls keine Rubrik "sonstige" vorhanden, aber andere vorhanden sind, wird diese erstellt
   if (!Object.keys(auswertung.byCategories).includes('sonstige') && Object.keys(auswertung.byCategories).length > 0) {
     categoryList.push('sonstige');
     rightList.push(0/*auswertung.byCategories[category].got*/)
@@ -179,8 +185,10 @@ for (category of Object.keys(neededTest.kategorien)) {
     // console.log(auswertung);
     auswertung.letter[selectedElementId.parent] = sortObjectByKey(auswertung.letter[selectedElementId.parent]);
     // categoryList = Object.keys(auswertung.letter[selectedElementId.parent]);
+    // Buchstaben, die in keiner Rubrik vorkamen werden nun (unter "sonstige") hinzugefügt
     for (letter of Object.keys(auswertung.letter[selectedElementId.parent])) {
       if (!wordsCountedTogether.includes(letter) && !(letterList.includes(letter[0])) && letter[0] != "e" && (letter != "<e>/<E>" || (!auswertung.byCategories.Endungen || !Object.keys(auswertung.byCategories.Endungen).includes("<e>/<E>")))) {
+        // Buchstabe mit Anzahl falsch und Anzahl richtig geschrieben hinzufügen
         categoryList.push(letter);
         // if (Object.keys(minusList).includes(letter[0])) {
           // auswertung.letter[selectedElementId.parent][letter].possible -= minusList[letter[0]].possible;
@@ -190,6 +198,7 @@ for (category of Object.keys(neededTest.kategorien)) {
         rightList.push(auswertung.letter[selectedElementId.parent][letter].got);
       }
     }
+    // _Silbige Wörter und wie oft sie falsch und wie oft sie richtig eingegeben wurden (unter "sonstige" ganz unten) hinzufügen
     for (auswertungNow of Object.keys(auswertung)) {
       if (auswertungNow.includes('Silben')) {
         categoryList.push(auswertungNow);
@@ -200,8 +209,8 @@ for (category of Object.keys(neededTest.kategorien)) {
     for (var i = 0; i < categoryList.length; i++) {
       mirrorList.push(0);
     }
+    // gespiegelte Buchstaben hinzufügen
     // TODO: <e> gespiegelt
-    // TODO: Buchstabe von gespiegel der, der angeklickt wurde und nicht der des korrekt geschriebenen Wortes an dem Index
     for (var i2 = 0; inputs[/*'Test ' + */selectedTest][selectedElementId.parent].mirror && i2 < Object.keys(inputs[/*'Test ' + */selectedTest][selectedElementId.parent].mirror).length; i2++) {
       for (var i4 = 0; i4 < inputs[/*'Test ' + */selectedTest][selectedElementId.parent].mirror[Object.keys(inputs[/*'Test ' + */selectedTest][selectedElementId.parent].mirror)[i2]].length; i4++) {
         if (inputs[/*'Test ' + */selectedTest][selectedElementId.parent].mirror[Object.keys(inputs[/*'Test ' + */selectedTest][selectedElementId.parent].mirror)[i2]][i4]) {
@@ -219,17 +228,20 @@ for (category of Object.keys(neededTest.kategorien)) {
       }
       }
       var doNotCountList = [];
+      // doNotCount (nict automatisch ausgewertet, also nur Graphemtreffer bekannt) zur Liste für die Grafik hinzufügen
       for (var i = 0; i < categoryList.length; i++) {
-        // normaler Buchstabe: 3 Zeichen
+        // Hinzufügen der Laute, falls in Rubrik vorhanden
         if (Object.keys(doNotCountObj.laute).includes(categoryList[i].split("/")[0])) {
           doNotCountList.push(doNotCountObj.laute[categoryList[i].split("/")[0]]);
         }
+        // Hinzufügen der Endung <e>
         else if (categoryList[i] == "<e>/<E>" && Object.keys(doNotCountObj).includes('<e>/<E>')) {
           doNotCountList.push(0);
           for (var i1 = 0; i1 < Object.keys(doNotCountObj[categoryList[i]]).length; i1++) {
             doNotCountList[doNotCountList.length - 1] += doNotCountObj[categoryList[i]][Object.keys(doNotCountObj[categoryList[i]])];
           }
         }
+        // Hinzufügen der nicht in Rubriken vorhandenen Buchstaben
         else if (Object.keys(doNotCountObj).includes(categoryList[i].split("/")[0])) {
           var total = 0;
           for (keyNow of Object.keys(doNotCountObj[categoryList[i].split("/")[0]])) {
@@ -237,25 +249,29 @@ for (category of Object.keys(neededTest.kategorien)) {
           }
           doNotCountList.push(total);
         }
+        // Eintragen, dass kein nicht auswertbarer vorhanden ist, oder deine Rubrik auf der y-Achse bei dem Index vorhanden ist, wodurch dort auch nichts vorhanden sein kann
         else if ((categoryList[i].length != 3 || !Object.keys(doNotCountObj).includes(categoryList[i][0])) && categoryList[i] != "<e>/<E>") {
           doNotCountList.push(0);
         }
+        // "sonstige" hinzufügen
         else if (categoryList[i].length == 3) {
           doNotCountList.push(0);
           // if (minusList.doNotCount[categoryList[i][0]] && minusList.doNotCount[categoryList[i][0]] != 0) {
+          // Abziehen wegen minusList (also Bucstaben aus Lauten (hier: mehrere Buchstaben) o.ä.)
             /*minusList.doNotCount[categoryList[i][0]]*/if (minusList.doNotCount[categoryList[i][0]]) doNotCountList[doNotCountList.length - 1] -= minusList.doNotCount[categoryList[i][0]];
           // }
           // else {
+          //
           for (wordNow of Object.keys(doNotCountObj[categoryList[i][0]])) {
             doNotCountList[doNotCountList.length - 1] += doNotCountObj[categoryList[i][0]][wordNow[i1]];
           }
         // }
         }
+        // sonst damit die richtige Position auf der y-achse erhalten bleibt, 0 hinzufügen
         else doNotCountList.push(0);
       }
     addChart(categoryList, {wrong: wrongList, right: rightList, doNotCount: doNotCountList, mirror: mirrorList}, printMode);
 }
-// TODO: Label größer
 // fügt einen neuen Graphem inzu bzw. aktuallisiert ihn
 // @param texte: Beschriftung des Graphen auf der y-Achse (Balkendiagramm)
 // @param data: Inhalte bzw. Daten für die Balken
@@ -377,7 +393,7 @@ document.getElementById('textur' + selectedElementId.parent).onclick = function(
           findChild('id', pupilSheet, 'correction ' + (i + 1)).style.outlineStyle = "outset";
           // TODO: leere Felder beachten (Liste der Class und dann bei leeren Feldern was rein setzen?)
           findChild('id', pupilSheet, 'correction ' + (i + 1)).style.width = graphemtrefferPossible[neededTest.words.length*(pupilSheet.replace('pupilSheet', '') - 1) + i].getBoundingClientRect().right + 3;
-          if (findChild('id', pupilSheet, 'pupilsWriting ' + (i + 1)).value.toLowerCase() == replaceAll(neededTest.words[i], '-', '').toLowerCase()) findChild('id', pupilSheet, 'correction ' + (i + 1)).style.width = 7*replaceAll(neededTest.words[i], '-', '').length;
+          // if (findChild('id', pupilSheet, 'pupilsWriting ' + (i + 1)).value.toLowerCase() == replaceAll(neededTest.words[i], '-', '').toLowerCase()) findChild('id', pupilSheet, 'correction ' + (i + 1)).style.width = 7*replaceAll(neededTest.words[i], '-', '').length;
         }
         }
       }
@@ -393,7 +409,7 @@ document.getElementById('textur' + selectedElementId.parent).onclick = function(
               if (auswertung.doNotCount[selectedElementId.parent].includes(wordNow)) findChild('word', pupilSheet, wordNow).style.outlineColor = "gray";
               findChild('word', pupilSheet, wordNow).style.outlineStyle = "outset";
               findChild('word', pupilSheet, wordNow).style.width = graphemtrefferPossible[neededTest.words.length*(pupilSheet.replace('pupilSheet', '') - 1) + i].getBoundingClientRect().right + 3;
-              if (findChild('id', pupilSheet, 'pupilsWriting ' + (i + 1)).value == wordNow) findChild('word', pupilSheet, wordNow).style.width = 7*replaceAll(neededTest.words[i], '-', '').length;
+              // if (findChild('id', pupilSheet, 'pupilsWriting ' + (i + 1)).value == wordNow) findChild('word', pupilSheet, wordNow).style.width = 7*replaceAll(neededTest.words[i], '-', '').length;
             }
           }
         }
